b986ee0740d38c05f74a5cd638c1ea27ba464a18
Pradeep Thapa <thapa.pradeepp@gmail.com>
2026-07-05 10:12:19 +0545

    feat: wire GoRouter into MaterialApp

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'core/navigation/app_router.dart';
 import 'core/theme/app_theme.dart';
-import 'features/transactions/presentation/screens/transaction_list_screen.dart';
 
 /// Root widget of the Koshly app.
 /// Responsible for theme and navigation configuration.
@@ -11,13 +11,13 @@ class KoshlyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Koshly',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
-      home: const TransactionListScreen(),
+      routerConfig: appRouter,
     );
   }
 }
